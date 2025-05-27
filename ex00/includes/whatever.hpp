f31f8e8d1c84d83b73b7940f90f08a876064ac75
Jonas <jonasper19@gmail.com>
2025-05-27 19:29:43 -0300

    first commit

---
+++ ex00/includes/whatever.hpp
@@ -0,0 +1,14 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+# define BRIGHT_RED "\033[1;31m"
+# define BRIGHT_BLUE "\033[1;34m"
+# define ORANGE "\033[38;5;208m"
+# define BRIGHT_GREEN "\033[1;32m"
+# define RESET "\033[0m"
+
+#include <iostream>
+
+
+
+#endif
